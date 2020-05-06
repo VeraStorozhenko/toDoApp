5e5ba85c10c6ce00d38a77fdfcd06092b8aea4a7
VeraStorozhenko <verun.mak@gmail.com>
2020-05-06 15:25:39 +0300

    fixed views with yield in application

--- test/controllers/list_controller_test.rb
+++ test/controllers/list_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class WelcomeControllerTest < ActionDispatch::IntegrationTest
+class ListControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get welcome_index_url
+    get list_index_url
     assert_response :success
   end
 
